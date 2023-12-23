81b6c9b639cec55ba579dac0dad27920f303aa5f
Ure8925 <suzzylicious24@gmail.com>
2023-12-23 22:44:10 +0100

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
+function showWeatherInfo(response){
+    let temperatureElement=document.querySelector("#temperature");
+    let temperature=response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML=response.data.city
+
+
+    temperatureElement.innerHTML=Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "302ea8odf48556tb65f5340850929f02";
+   let apiUrl =
+     `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(showWeatherInfo);
+}
+
 function handleSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-details");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmit);
+searchCity("Lagos");
