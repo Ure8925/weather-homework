1db6edfed7265d3841f8037e73400cf49312e7d7
Ure8925 <suzzylicious24@gmail.com>
2023-12-27 03:03:14 +0100

    injected the weather icon url

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ function showWeatherInfo(response){
     let windElement = document.querySelector("#wind");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000 );
-    
+    let iconElement = document.querySelector("#icon");
+
+   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
     timeElement.innerHTML = newDate(date);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
